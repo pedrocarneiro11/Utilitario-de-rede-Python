589eb7412d8b71a6e9a0cbd584d448891b0a4881
pedrocarneiro11 <pedrocarneirocunha87@gmail.com>
2024-04-13 15:35:14 -0300

    commit

---
+++ flush.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+import os
+import pyuac
+from pyuac import main_requires_admin
+
+@main_requires_admin
+def main():
+    try:
+        subprocess.call(["ipconfig", "/release"], shell=True) # ok
+        # subprocess.call(["ipconfig", "/renew"], shell=True) # ok
+        # subprocess.call(["ipconfig", "/flushdns"], shell=True) # ok
+        # subprocess.call(["runas", "/user:Administrator", "ipconfig", "/registerdns"], shell=True)
+        # subprocess.call(["ipconfig", "/nbtstat -rr"], shell=True)
+        # subprocess.call(["ipconfig", "/netsh int ip reset all"], shell=True)
+        # subprocess.call(["ipconfig", "/netsh winsock reset"], shell=True)
+        input("Press enter to close the window. >")
+    except Exception:
+        print("error")
+
+if __name__ == "__main__":
+    main()
+
+
