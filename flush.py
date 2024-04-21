6d793b77676f9c4a540f9a8ea6bf5d5498d8db5f
pedrocarneiro11 <pedrocarneirocunha87@gmail.com>
2024-04-21 14:22:29 -0300

    nbtstat -rr e gitignore ok

--- flush.py
+++ flush.py
@@ -3,17 +3,26 @@ import sys
 import os
 import pyuac
 from pyuac import main_requires_admin
+import socket
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
 
 @main_requires_admin
 def main():
+    nbtstat_command = r'C:\Windows\Sysnative\nbtstat.exe'
     try:
         # subprocess.call(["ipconfig", "/release"], shell=True) # ok
         # subprocess.call(["ipconfig", "/renew"], shell=True) # ok  
         # subprocess.call(["ipconfig", "/flushdns"], shell=True) # ok  
-        subprocess.call(["ipconfig", "/registerdns"], shell=True)
-        # subprocess.call(["ipconfig", "/nbtstat -rr"], shell=True)
-        # subprocess.call(["ipconfig", "/netsh int ip reset all"], shell=True)
-        # subprocess.call(["ipconfig", "/netsh winsock reset"], shell=True)
+        # subprocess.call(["ipconfig", "/registerdns"], shell=True) # ok 
+        # print(local_ip)        
+        # result = os.popen('nbtstat -rr').read() # ok
+        # print(result) # ok
+        # output, _ = p.communicate() # Erro
+        # print(output.decode('utf-8'))
+        # subprocess.call(["netsh int ip reset all"], shell=True)
+        # subprocess.call(["netsh winsock reset"], shell=True)
         input("Press enter to close the window. >")
     except Exception:
         print("error")
